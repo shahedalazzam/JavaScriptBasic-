a992b6c4b88287dbd867079d0cb9465174b105b1
shahedalazzam <shahed.a.alazzam@gmail.com>
2023-04-26 15:12:05 +0300

    task

---
+++ java.js
@@ -0,0 +1,76 @@
+// Q1
+
+var Cash= 1000/500
+console.log(Cash)
+
+var income= 1000-500
+console.log(income)
+
+var assets=1000+500
+console.log(assets)
+
+var income=1000*500
+console.log(income)
+
+var Average=(7+9+2)/3
+console.log(Average)
+
+var Discount=(150*30/100)
+console.log(150-Discount)
+
+var age=20
+if (age>18 && age<30) {
+    console.log(true)
+}
+else{
+    console.log(false)
+}
+var num1=2
+var num2=3
+console.log(num1**num2)
+
+var num1=10
+var num2=4
+console.log(num1%num2)
+
+
+
+// Q2
+
+console.log(typeof(100))
+console.log(typeof(73.9))
+console.log(typeof(NaN))
+console.log(typeof("Water"))
+console.log(typeof(false))
+console.log(typeof(9 != 11))
+console.log("Orange"+"e")
+console.log("Orange"-"s")
+console.log("4"+"8")
+console.log("4"-"8")
+console.log("name" + 3)
+console.log("name" - 3)
+console.log(82 * "word")
+console.log(1 + "hello")
+console.log(1 + true)
+console.log("hello" + true)
+console.log(typeof (Infinity))
+console.log(1 == '1')
+console.log(1 === '1')
+
+
+// Q3
+
+console.log("WELCOME TO ORANGE")
+console.log("TO")
+console.log(" Hello from Orange")
+console.log(" welcome to orange")
+console.log("17")
+console.log("Welcome to “Orange”")
+console.log("Welcome to Orange Jordan")
+
+
+//Q4
+
+const str = 'cactus';
+const y = str.replaceAll(str[0], '*');
+console.log(str[0]+y.slice(1));
